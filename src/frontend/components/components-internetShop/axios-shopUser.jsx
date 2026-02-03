ab700c80ba37725740f2960d2af7a8faaca84dba
Lexavorio1 <leoboy99@mail.ru>
2026-02-03 21:58:41 +0300

    1 commit

---
+++ src/frontend/components/components-internetShop/axios-shopUser.jsx
@@ -0,0 +1,26 @@
+
+/*
+import axios from 'axios'
+
+export const api = axios.create({
+  baseURL: 'http://localhost:2026/api'
+})
+
+api.interceptors.request.use(config => {
+  const token =
+    localStorage.getItem('token') ||
+    sessionStorage.getItem('token')
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
+*/
+
+import axios from 'axios'
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+})
